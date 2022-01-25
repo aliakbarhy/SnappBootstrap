cc538c21403a5eb68ce428ad6c091e127b6ffe4b
aliakbarhy <75787729+aliakbarhy@users.noreply.github.com>
2022-01-25 15:46:50 +0330

    aa2

--- js/MyJs.js
+++ js/MyJs.js
@@ -5,7 +5,13 @@ $(document).ready(function(){
   $("#myBtn").click(function(){
    $("#myCarousel").carousel("prev");
 });
-$("#showmore").click(function(){
-  console.log("hello");
-}); 
+
+$('#showmore').on('click', function () {
+// For multiple css property
+$('#showmore').css('display','none');
+$('#fourthSec').css('margin-top','10%');
+$('#fourthSec').css('-webkit-box-shadow','none');
+$('#fourthSec').css('box-shadow','none');
+});
+
 });
